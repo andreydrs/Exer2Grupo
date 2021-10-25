0bbbf74ca8c7bb0b4cbba751ee03e150cd92f235
andrey <andrey@ANDREY-PC>
2021-10-25 13:55:13 -0300

    Adicionar arquivos de projeto.

---
+++ Exer2Grupo/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exer2Grupo
+{
+    class Funcionario
+	{
+		private string Nome = "Joao Silva";
+        private double SalarioBruto = 6000;
+		private double Imposto = 1000;
+
+		public void salarioLiquido()
+		{
+			double salario = SalarioBruto-Imposto;
+			Console.WriteLine("\nFuncionario: {0} $ {1}",Nome, salario.ToString("F2"));
+
+		}
+
+		public void aumentarSalario()
+		{
+			double aumento = 10;
+			double salarioAtualizado = SalarioBruto * 0.10 + SalarioBruto-Imposto;
+			
+			Console.WriteLine("\nDigite a percentagem para aumentar o salario: {0}", aumento.ToString("F1"));
+			Console.WriteLine("\nDados Atualizado: {0} $ {1}",Nome, salarioAtualizado.ToString("F2"));
+		}
+
+	}
+}
---
+++ Exer2Grupo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exer2Grupo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Funcionario p1 = new Funcionario();
+            p1.salarioLiquido();
+            p1.aumentarSalario();
+
+            
+        }
+    }
+}
